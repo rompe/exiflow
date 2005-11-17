dde0e5ce45b9651174875def0e16df18e2425cc2
Ulf Rompe <ulf@rompe.org>
2005-11-17 17:28:25 +0000

    Add some methods to the filelist class.

--- exiflow/exiflow/filelist.py
+++ exiflow/exiflow/filelist.py
@@ -23,6 +23,16 @@ class Filelist:
       self._filestats = {}
       self._fullsize = 0
       self._process_unknown_types = process_unknown_types
+      self.add_files(pathes)
+
+
+   def add_files(self, *pathes):
+      """
+      Add filenames found in *pathes either to _files or _skippedfiles,
+      depending on their extension and the value of _process_unknown_types.
+      Return False if files are skipped or no files are processed,
+      True otherwise.
+      """
       for path in pathes:
          if os.path.isfile(path):
             self.add_file(path)
@@ -32,14 +42,6 @@ class Filelist:
                   self.add_file(os.path.join(root, basefile))
          else:
             raise IOError, path + " is not a regular file or directory."
-
-
-   def add_file(self, filename):
-      """
-      Add a filename either to _files or _skippedfiles, depending on
-      it's extension and the value of self._process_unknown_types.
-      Return True for files to process and False for skipped files.
-      """
       if self._process_unknown_types == True or \
          os.path.splitext(filename)[1] in self.knownextensions:
          self._files.append(filename)
@@ -52,3 +54,35 @@ class Filelist:
       else:
          self._skippedfiles.append(filename)
          return False
+
+
+   def __iter__(self):
+      """
+      Iterate through filelist and update percentage.
+      Returns (filename, percentage) pairs on each iteration.
+      """
+      size_so_far = 0
+      for filename in self._files:
+         size_so_far += self.filestats[filename].st_size
+         yield(filename, 100 / self._fullsize * size_so_far)
+
+
+   def get_daterange(self):
+      """
+      Get the date range of the files as a string in the form
+      YYYY-MM-DD_-_YYYY-MM-DD   or  YYYY-MM-DD  if only one date available.
+      """
+      dates = sorted(set(self._filedates.values()))
+      range_string = dates[0]
+      if len(dates) > 1:
+         range_string += "_-_" + dates.pop()
+      return range_string
+
+
+   def get_date(self, filename):
+      """
+      Get modification date for filename.
+      """
+      return self._filedates[filename]
+
+
