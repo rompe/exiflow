134cf4db7e24e59dd740cdcbb855cf7123aa5cc4
Ulf Rompe <ulf@rompe.org>
2005-11-17 18:31:27 +0000

    Complete filelist class, let exiimport.py use it.

--- exiflow/exiflow/filelist.py
+++ exiflow/exiflow/filelist.py
@@ -5,17 +5,17 @@ A class that hosts information about files given in it's contructor.
 """
 
 import os
+import time
 
 class Filelist:
 
    knownextensions = (".jpg", ".nef", ".raw", ".crw", ".tif")
 
-   def __init__(self, process_unknown_types=False, *pathes)
+   def __init__(self, process_unknown_types=False, *pathes):
       """
-      Walk through directories and files given as arguments and call
-      add_file() for all found files.
+      Create FileList object and set _process_unknown_types to given value.
+      Call add_files() with all remaining arguments.
       If process_unknown_types is given, then no files will be skipped.
-      Raises IOError on access errors.
       """
       self._files = []
       self._skippedfiles = []
@@ -26,34 +26,41 @@ class Filelist:
       self.add_files(pathes)
 
 
-   def add_files(self, *pathes):
+   def add_files(self, pathes):
       """
-      Add filenames found in *pathes either to _files or _skippedfiles,
+      Add filenames found in pathes either to _files or _skippedfiles,
       depending on their extension and the value of _process_unknown_types.
       Return False if files are skipped or no files are processed,
       True otherwise.
+      Raises IOError on access errors.
       """
+      found_known = False
+      found_unknown = False
+      filelist = []
       for path in pathes:
+         print "path:", path
          if os.path.isfile(path):
-            self.add_file(path)
+            filelist.append(path)
          elif os.path.isdir(path):
             for root, dirs, files in os.walk(path):
                for basefile in files:
-                  self.add_file(os.path.join(root, basefile))
+                  filelist.append(os.path.join(root, basefile))
          else:
             raise IOError, path + " is not a regular file or directory."
-      if self._process_unknown_types == True or \
-         os.path.splitext(filename)[1] in self.knownextensions:
-         self._files.append(filename)
-         filestat = os.stat(filename)
-         self._filestats[filename] = filestat
-         self._fullsize += filestat.st_size
-         filedate = time.strftime("%Y-%m-%d", time.localtime(filestat.st_mtime))
-         self._filedates[filename] = filedate
-         return True
-      else:
-         self._skippedfiles.append(filename)
-         return False
+      for filename in filelist:
+         if self._process_unknown_types == True or \
+            os.path.splitext(filename)[1] in self.knownextensions:
+            found_known = True
+            self._files.append(filename)
+            filestat = os.stat(filename)
+            self._filestats[filename] = filestat
+            self._fullsize += filestat.st_size
+            filedate = time.strftime("%Y-%m-%d", time.localtime(filestat.st_mtime))
+            self._filedates[filename] = filedate
+         else:
+            found_unknown = True
+            self._skippedfiles.append(filename)
+      return found_known and not found_unknown
 
 
    def __iter__(self):
@@ -63,8 +70,8 @@ class Filelist:
       """
       size_so_far = 0
       for filename in self._files:
-         size_so_far += self.filestats[filename].st_size
-         yield(filename, 100 / self._fullsize * size_so_far)
+         size_so_far += self._filestats[filename].st_size
+         yield(filename, 100 * size_so_far / self._fullsize)
 
 
    def get_daterange(self):
@@ -85,4 +92,17 @@ class Filelist:
       """
       return self._filedates[filename]
 
+
+   def get_fullsize(self):
+      """
+      Get cumulated bytes of files.
+      """
+      return self._fullsize
+
+
+   def get_filecount(self):
+      """
+      Get number of files.
+      """
+      return len(self._files)
 
--- exiflow/exiimport.py
+++ exiflow/exiimport.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python2.4
 # -*- coding: utf-8 -*-
+"""
+Import files from given directories to your photo folder.
+Optionally unmounts source media after successfull import.
+"""
+
 import os
 import sys
 import stat
-import time
 import shutil
 import optparse
 import subprocess
-
-extensions = (".jpg", ".nef", ".raw", ".gif")
+import exiflow.filelist
 
 # Parse command line.
 parser = optparse.OptionParser()
@@ -24,54 +27,30 @@ parser.add_option("-d", "--device", dest="device",
                        " to %d in the gnome-volume-manager config dialog.")
 options, args = parser.parse_args()
 if len(args) > 0 or not options.mount or not options.target:
-    print >>sys.stderr, "Wrong syntax, call with --help for info."
-    sys.exit(1)
+    sys.exit("Wrong syntax, call with --help for info.")
 
-# Build dictionary of images to import.
-# Every entry has the complete path as the key and a corresponding stat object
-# as it's value, so we can easily retrieve st_mtime or st_size from it.
-images = {}
-fullsize = 0
-datedict = {}
-for root, dirs, files in os.walk(options.mount):
-    for name in files:
-        if name.endswith(".dsc"):
-            print "Skipping file:", name
-        else:
-            filepath = os.path.join(root, name)
-            filestat = os.stat(filepath)
-            images[filepath] = filestat
-            datedict[time.strftime("%Y-%m-%d",
-                                   time.localtime(filestat.st_mtime))] = True
-            fullsize += filestat.st_size
+# Build file list whithout skipping unknown files
+filelist = exiflow.filelist.Filelist(True, options.mount)
 
 # Cry if we found no images
-if fullsize == 0:
-    print >>sys.stderr, "No bytes to import, sorry."
-    sys.exit(1)
-
-# Make up daterange
-tmpdates = datedict.keys()
-tmpdates.sort()
-daterange = tmpdates[0]
-tmplen = len(tmpdates)
-if tmplen > 1:
-    daterange += "_-_" + tmpdates[tmplen - 1]
+if filelist.get_filecount() == 0:
+    sys.exit("No files to import, sorry.")
 
 # Create targetdir
-targetdir = os.path.join(options.target, daterange)
+targetdir = os.path.join(options.target, filelist.get_daterange())
+# TODO: find a better solution than just appendings "+" chars.
 while os.path.exists(targetdir):
     targetdir += "+"
 os.makedirs(targetdir)
-print "Importing", fullsize / 1024 / 1024 , "MB to", targetdir
+print "Importing", filelist.get_fullsize() / 1024 / 1024 , "MB in", \
+      filelist.get_filecount(), "files to", targetdir
 
 # Copy files
 progress_size = 0
-for file in images:
-   progress_size += images[file].st_size
-   print "%3s%% (%s)" % (100 * progress_size / fullsize, file)
-   shutil.copy2(file, targetdir)
-   os.chmod(os.path.join(targetdir, os.path.basename(file)), stat.S_IMODE(0644))
+for filename, percentage in filelist:
+   print "%3s%% %s" % (percentage, filename)
+   shutil.copy2(filename, targetdir)
+   os.chmod(os.path.join(targetdir, os.path.basename(filename)), stat.S_IMODE(0644))
 
 # Unmount card
 if options.device:
