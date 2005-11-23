fe15946183f71cf02af3317c16ba8215c6e03bc1
Sebastian Berthold <sb@sleif.de>
2005-11-23 23:22:26 +0000

    - new configfile handling for exif.cfg and cameras.cfg

--- exiflow/exiflow/configfile.py
+++ exiflow/exiflow/configfile.py
@@ -34,3 +34,62 @@ def settings():
    config.write(open(local_config, "w"))
    return config, read_files
 
+def cameras():
+   """
+   Handle access to cameras.cfg.
+   Return a configparser object and a list of processed files.
+   """
+   local_config = os.path.join(local_config_dir, "cameras.cfg")
+   global_config = os.path.join(global_config_dir, "cameras.cfg")
+   config = ConfigParser.ConfigParser()
+# Create some defaults
+   config.add_section("all")
+   config.set("all", "cam_id", "000")
+   config.set("all", "artist_initials", "xy")
+
+   config.add_section("NIKON D70")
+   config.set("NIKON D70", "cam_id", "n00")
+
+   config.add_section("HP PhotoSmart 715")
+   config.set("HP PhotoSmart 715", "cam_id", "hp0")
+
+   config.add_section("C2040Z")
+   config.set("C2040Z", "cam_id", "o00")
+# Eventually read config files
+   read_files = config.read([global_config, local_config])
+# Write private config file (unconditionally, in case we have new options)
+   if not os.path.isdir(local_config_dir):
+      os.makedirs(local_config_dir)
+   config.write(open(local_config, "w"))
+   return config, read_files
+
+def exif():
+   """
+   Handle access to exif.cfg.
+   Return a configparser object and a list of processed files.
+   """
+   local_config = os.path.join(local_config_dir, "exif.cfg")
+   global_config = os.path.join(global_config_dir, "exif.cfg")
+   config = ConfigParser.ConfigParser()
+# Create some defaults
+   config.add_section("all")
+   config.set("all", "Artist", "xxx yyy")
+   config.set("all", "Contact", "xy@example.org")
+   config.set("all", "Copyright", "(c)2005 xy@example.org")
+   config.set("all", "CopyrightNotice", "All rights reserved, no redistribution without written permission.")
+   config.set("all", "Credit", "xy@example.org")
+   config.set("all", "UserComment", "")
+
+   config.add_section("custom")
+   config.set("custom", "Artist", "custom xxx yyy")
+
+   config.add_section("NIKON D70")
+   config.set("NIKON D70", "Artist", "custom xxx yyy for this camera")
+# Eventually read config files
+   read_files = config.read([global_config, local_config])
+# Write private config file (unconditionally, in case we have new options)
+   if not os.path.isdir(local_config_dir):
+      os.makedirs(local_config_dir)
+   config.write(open(local_config, "w"))
+   return config, read_files
+
