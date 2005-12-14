099e1c8ead917d572e59531762a763c523feeae9
Ulf Rompe <ulf@rompe.org>
2005-12-14 21:24:37 +0000

    Add write() method to Gthumb class.

--- exiflow/exiflow/gthumb.py
+++ exiflow/exiflow/gthumb.py
@@ -24,10 +24,12 @@ class Gthumb:
                                        os.path.basename(self.filename) + ".xml")
 
 
-   def read():
+   def read(self):
       """
-      Read gthumb comment information for filename and return a dictionary
-      containing the corresponding EXIF values.
+      Read gthumb comment information for filename and fill the dictionary
+      self.fields with the corresponding EXIF values.
+      Return True on success and False if no comment file exists.
+
       Translation of Exif values to gthumb sections:
 
           "Artist"            => "Note" line: "^Artist::.*"
@@ -75,5 +77,54 @@ class Gthumb:
          return True
       else:
          return False
+
+
+   def write(self, myaddfields=False, mytemplate=False):
+      """
+      Write Exif information from self.fields into gthumb comment file.
+      Apply reverse mapping of self.read().
+
+      myaddfields: Write some additional fields.
+      mytemplate: Write empty fields as well.
+      """
+      exiffields = ["Artist", "Credit", "Copyright", "CopyrightNotice", "UserComment"]
+      filename = os.path.abspath(filename)
+      commentsdir = os.path.join(os.path.dirname(filename), ".comments")
+      myxmlfile = os.path.join(commentsdir, os.path.basename(filename) + ".xml")
+      mydata = {}
+      mydata["Place"] = self.fields.get("Location", "")
+      mydata["Time"] = self.fields.get("DateTimeOriginal", "")
+      mydata["Note"] = self.fields.get("ImageDescription", "")
+      tmpkeywords = []
+      for keyword in self.fields.get("Keywords", "").split(","):
+         tmpkeywords.append(keyword.strip())
+      mydata["Keywords"] = ",".join(tmpkeywords)
+
+# Add XPTitle to Note if Note doesn't contain it already
+      mytitle = self.fields.get("XPTitle", "")
+      if len(mytitle) > 0 and not mytitle in mydata["Note"].splitlines():
+         mydata["Note"] = self.fields["XPTitle"] + "\n" + mydata["Note"]
+      if myaddfields or mytemplate:
+         mydata["Note"] += "\n"
+         for field in exiffields:
+            fieldvalue = self.fields.get(field, "")
+            if mytemplate or len(fieldvalue) > 0:
+               mydata["Note"] += "\n" + field + ":: " + fieldvalue
+
+      mydom = xml.dom.minidom.Document()
+      comment = mydom.createElement("Comment")
+      comment.setAttribute("format", "2.0")
+      mydom.appendChild(comment)
+
+      for name in ("Place", "Time", "Note", "Keywords"):
+         element = mydom.createElement(name)
+         text = mydom.createTextNode(mydata[name])
+         element.appendChild(text)
+         comment.appendChild(element)
+
+      if not os.path.isdir(commentsdir):
+         os.makedirs(commentsdir)
+      gzip.open(myxmlfile, "wb").write(mydom.toxml(encoding="utf-8"))
+
 
 
