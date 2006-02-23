f9881d6321c8530095c59526366939857022bfae
Ulf Rompe <ulf@rompe.org>
2006-02-23 09:21:11 +0000

    Set version to 0.1.0 and add metadata to setup.py to prepare announcing at the Python Cheeseshop.

--- exiflow/setup.py
+++ exiflow/setup.py
@@ -7,7 +7,7 @@ Install the exiflow collection on your computer.
 import distutils.core
 
 distutils.core.setup(name='exiflow',
-                     version='0.1',
+                     version='0.1.0',
                      scripts=['exigui',
                               'exiassign',
                               'exigate',
@@ -15,5 +15,34 @@ distutils.core.setup(name='exiflow',
                               'exiperson',
                               'exirename'],
                      packages=['exiflow'],
-                     package_data={'exiflow': ['*.glade']})
+                     package_data={'exiflow': ['*.glade']},
+                     author='Ulf Rompe, Sebastian Berthold',
+                     author_email='exiflow@rompe.org',
+                     url='http://sourceforge.net/projects/exiflow/',
+                     description='A set of tools including a little GUI to '
+                                 'provide a complete digital photo workflow '
+                                 'for Unixes, using EXIF headers as the '
+                                 'central information repository.',
+                     long_description='A set of tools including a little GUI '
+                                      'to provide a complete digital photo '
+                                      'workflow for Unixes. EXIF headers are '
+                                      'used as the central information '
+                                      'repository, so users may change their '
+                                      'software at any time without loosing '
+                                      'their data. The tools may be used '
+                                      'individually or combined.',
+                     download_url='http://sourceforge.net/project/showfiles.php?group_id=151136',
+                     classifiers=['Development Status :: 3 - Alpha',
+                                  'Environment :: Console',
+                                  'Environment :: X11 Applications :: GTK',
+                                  'Intended Audience :: End Users/Desktop',
+                                  'License :: OSI Approved :: GNU General Public License (GPL)',
+                                  'Operating System :: POSIX',
+                                  'Operating System :: POSIX :: Linux',
+                                  'Operating System :: Unix',
+                                  'Programming Language :: Python',
+                                  'Topic :: Multimedia :: Graphics',
+                                  'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+                                  'Topic :: System :: Archiving',
+                                  'Topic :: Utilities'])
 
