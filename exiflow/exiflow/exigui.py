f26b41ef5caec43afaa97ec69c3f617456eca17c
Ulf Rompe <ulf@rompe.org>
2006-06-24 22:20:29 +0000

    More cleanup and refactoring of exigui.py. Pylint now gives 6.30. :-/

--- exiflow/exiflow/exigui.py
+++ exiflow/exiflow/exigui.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2.4
 # -*- coding: UTF-8 -*-
+"""
+A nice PyGTK GUI for the exiflow tool collection.
+"""
+__revision__ = "$Id$"
 
 import os.path
 import sys
@@ -36,11 +40,11 @@ class WritableTextView:
          self.tag_names.append(color)
 
    def write(self, msg):		
-      iter = self.buffer.get_end_iter()
+      my_iter = self.buffer.get_end_iter()
       tag_names = self.tag_names[:]
       if msg.startswith("WARNING") or msg.startswith("ERROR"):
          tag_names.append("warning")
-      self.buffer.insert_with_tags_by_name(iter, msg, *tag_names)
+      self.buffer.insert_with_tags_by_name(my_iter, msg, *tag_names)
       self.textview.scroll_mark_onscreen(self.buffer.get_insert())
 
 
@@ -106,7 +110,8 @@ class Window1(object):
       text_column = gtk.TreeViewColumn("Filename")
       text_column.pack_start(text_cell, True)
       text_column.add_attribute(text_cell, "text", 0)
-      #text_column.set_attributes(text_cell, markup=1) #wir müssen markup anschalten um den text später formatiern zu können
+      #text_column.set_attributes(text_cell, markup=1)
+      #wir müssen markup anschalten um den text später formatieren zu können
       treeview.append_column(text_column)
       dic = {}
       for key in dir(self.__class__):
@@ -117,19 +122,37 @@ class Window1(object):
       sys.stdout = WritableTextView(self.wTree.get_widget("textview1"))
       sys.stderr = WritableTextView(self.wTree.get_widget("textview1"), "blue")
 
-   def on_button_open_clicked(self, widget, data = None):
+   def _make_sensitive(self, name):
+      """ Set widget called name sensitive. """
+      self.wTree.get_widget(name).set_sensitive(True)
+
+   def _make_insensitive(self, name):
+      """ Set widget called name insensitive. """
+      self.wTree.get_widget(name).set_sensitive(False)
+
+   def _is_active(self, name):
+      """ Retrun True if widget "name" is activated, False otherwise. """
+      return self.wTree.get_widget(name).get_active()
+
+   def set_text(self, name, text):
+      """ Set text of widget "name" to text. """
+      self.wTree.get_widget(name).set_text(text)
+
+   def on_button_open_clicked(self, *dummy):
       diag = Filechooser1(self.window, self.set_filelist)
 
-   def on_button_exiimport_browse_importdir_clicked(self, widget, data = None):
-      diag = Directorychooser1(self.window, self.wTree.get_widget("exiimport_importdir_entry").set_text)
+   def on_button_exiimport_browse_importdir_clicked(self, *dummy):
+      diag = Directorychooser1(self.window,
+               self.wTree.get_widget("exiimport_importdir_entry").set_text)
 
-   def on_button_exiimport_browse_targetdir_clicked(self, widget, data = None):
-      diag = Directorychooser1(self.window, self.wTree.get_widget("exiimport_targetdir_entry").set_text)
+   def on_button_exiimport_browse_targetdir_clicked(self, *dummy):
+      diag = Directorychooser1(self.window,
+               self.wTree.get_widget("exiimport_targetdir_entry").set_text)
 
-   def on_info1_activate(self, widget, data=None):
+   def on_info1_activate(self, *dummy):
       diag = Aboutdialog1(self.window)
 
-   def on_mainwindow_destroy(self, widget, data = None):
+   def on_mainwindow_destroy(self, *dummy):
       gtk.main_quit()
 
    def set_filelist(self, files):
@@ -139,27 +162,28 @@ class Window1(object):
          if os.path.exists(filename):
             self.liststore.append([filename])
          else:
-            print >>sys.stderr, filename, "doesn't exist!"
+            print >> sys.stderr, filename, "doesn't exist!"
 
-   def on_exirename_camid_auto_activate(self, widget, data=None):
-      self.wTree.get_widget("exirename_cam_id_entry").set_sensitive(False)
+   def on_exirename_camid_auto_activate(self, *dummy):
+      self._make_insensitive("exirename_cam_id_entry")
 
-   def on_exirename_camid_custom_activate(self, widget, data=None):
-      self.wTree.get_widget("exirename_cam_id_entry").set_sensitive(True)
+   def on_exirename_camid_custom_activate(self, *dummy):
+      self._make_sensitive("exirename_cam_id_entry")
 
-   def on_exirename_artist_auto_activate(self, widget, data=None):
-      self.wTree.get_widget("exirename_artist_initials_entry").set_sensitive(False)
+   def on_exirename_artist_auto_activate(self, *dummy):
+      self._make_insensitive("exirename_artist_initials_entry")
 
-   def on_exirename_artist_custom_activate(self, widget, data=None):
-      self.wTree.get_widget("exirename_artist_initials_entry").set_sensitive(True)
+   def on_exirename_artist_custom_activate(self, *dummy):
+      self._make_sensitive("exirename_artist_initials_entry")
 
-   def on_exiperson_section_auto_activate(self, widget, data=None):
-      self.wTree.get_widget("exiperson_section_entry").set_sensitive(False)
+   def on_exiperson_section_auto_activate(self, *dummy):
+      self._make_insensitive("exiperson_section_entry")
 
-   def on_exiperson_section_custom_activate(self, widget, data=None):
-      self.wTree.get_widget("exiperson_section_entry").set_sensitive(True)
+   def on_exiperson_section_custom_activate(self, *dummy):
+      self._make_sensitive("exiperson_section_entry")
 
-   def _progress_callback(self, filename, newname, percentage, keep_original=False):
+   def _progress_callback(self, filename, newname, percentage,
+                          keep_original=False):
       """
       This callback is given as a callable to the main programs and is
       called after each processed file. filename and newname may of course
@@ -181,7 +205,7 @@ class Window1(object):
          gtk.main_iteration(False)
       return self._cancelled
       
-   def on_cancel_activate(self, widget, data=None):
+   def on_cancel_activate(self, widget, *dummy):
       """
       Called from the cancel button.
       """
@@ -189,7 +213,7 @@ class Window1(object):
       widget.set_sensitive(False)
       sys.stderr.write("CANCELLED!\n")
 
-   def on_run_activate(self, widget, data=None):
+   def on_run_activate(self, widget, *dummy):
       """
       Called from the run button.
       """
@@ -200,7 +224,8 @@ class Window1(object):
       widget.set_sensitive(False)
       self._cancelled = False
       
-      label = nbook.get_tab_label(nbook.get_nth_page(nbook.get_current_page())).get_text()
+      ntab = nbook.get_tab_label(nbook.get_nth_page(nbook.get_current_page()))
+      label = ntab.get_text()
       sys.stderr.write("Running %s\n" % label)
       method = getattr(self, "run_" + label.replace(" ", "_"))
       method()
@@ -210,12 +235,13 @@ class Window1(object):
       widget.set_sensitive(True)
 
    def run_exirename(self):
+      """ Run exirename. """
       args = ["-v"]
       artist_initials = self.wTree.get_widget("exirename_artist_initials_entry")
       cam_id = self.wTree.get_widget("exirename_cam_id_entry")
-      if self.wTree.get_widget("exirename_artist_initials_entry_button_custom").get_active():
+      if self._is_active("exirename_artist_initials_entry_button_custom"):
          args.append("--artist_initials=" + artist_initials.get_text())
-      if self.wTree.get_widget("exirename_cam_id_entry_button_custom").get_active():
+      if self._is_active("exirename_cam_id_entry_button_custom"):
          args.append("--cam_id=" + cam_id.get_text())
       args += [entry[0] for entry in self.liststore]
       try:
@@ -224,9 +250,9 @@ class Window1(object):
          sys.stdout.write("\nERROR: %s\n" % str(msg))
 
    def run_exiassign(self):
+      """ Run exiassign. """
       args = ["-v"]
-      forcebutton = self.wTree.get_widget("exiassign_force_checkbutton")
-      if forcebutton.get_active() == True:
+      if self._is_active("exiassign_force_checkbutton"):
          args.append("--force")
       args += [entry[0] for entry in self.liststore]
       try:
@@ -235,9 +261,10 @@ class Window1(object):
          sys.stdout.write("\nERROR: %s\n" % str(msg))
 
    def run_exiperson(self):
+      """ Run exiperson. """
       args = ["-v"]
       exif_section = self.wTree.get_widget("exiperson_section_entry")
-      if self.wTree.get_widget("exiperson_section_entry_button_custom").get_active():
+      if self._is_active("exiperson_section_entry_button_custom"):
          args.append("--section=" + exif_section.get_text())
       args += [entry[0] for entry in self.liststore]
       try:
@@ -246,13 +273,14 @@ class Window1(object):
          sys.stdout.write("\nERROR: %s\n" % str(msg))
       
    def run_exigate_gthumb(self):
-      if self.wTree.get_widget("exigate_gthumb_nooptions").get_active() == True:
+      """ Run exigate. """
+      if self._is_active("exigate_gthumb_nooptions"):
          args = ["-v"]
-      if self.wTree.get_widget("exigate_gthumb_addfields").get_active() == True:
+      if self._is_active("exigate_gthumb_addfields"):
          args = ["--additional-fields"]
-      if self.wTree.get_widget("exigate_gthumb_template").get_active() == True:
+      if self._is_active("exigate_gthumb_template"):
          args = ["--template"]
-      if self.wTree.get_widget("exigate_gthumb_cleanup").get_active() == True:
+      if self._is_active("exigate_gthumb_cleanup"):
          args = ["--cleanup"]
       args.append("-v")
       args += [entry[0] for entry in self.liststore]
@@ -262,6 +290,7 @@ class Window1(object):
          sys.stdout.write("\nERROR: %s\n" % str(msg))
 
    def run_exiimport(self):
+      """ Run exiimport. """
       args = ["-v"]
       import_dir = self.wTree.get_widget("exiimport_importdir_entry")
       device = self.wTree.get_widget("exiimport_device_entry")
@@ -278,28 +307,32 @@ class Window1(object):
          sys.stdout.write("\nERROR: %s\n" % str(msg))
 
 def run(argv):
+   """
+   Take an equivalent of sys.argv[1:] and run the GUI.
+   """
    parser = optparse.OptionParser()
    parser.add_option("-m", "--mount", dest="mount",
-                     help="Mountpoint of directory to import. Corresponds" \
+                     help="Mountpoint of directory to import. Corresponds"
                           " to %m in the gnome-volume-manager config dialog.")
    parser.add_option("-t", "--target", dest="target",
-                     help="Target directory. A subdirectory will be created" \
+                     help="Target directory. A subdirectory will be created"
                           " in this directory.")
    parser.add_option("-d", "--device", dest="device",
-                     help="Mounted device file. If given, this device will be " \
-                          "unmounted using pumount after the import. Corresponds" \
-                          " to %d in the gnome-volume-manager config dialog.")
+                     help="Mounted device file. If given, this device will be "
+                          "unmounted using pumount after the import. "
+                          "Corresponds to %d in the gnome-volume-manager "
+                          "config dialog.")
    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
                      help="Be verbose.")
    options, args = parser.parse_args(argv)
 
    win1 = Window1()
    if options.mount:
-      win1.wTree.get_widget("exiimport_importdir_entry").set_text(options.mount)
+      win1.set_text("exiimport_importdir_entry", options.mount)
    if options.device:
-      win1.wTree.get_widget("exiimport_device_entry").set_text(options.device)
+      win1.set_text("exiimport_device_entry", options.device)
    if options.target:
-      win1.wTree.get_widget("exiimport_targetdir_entry").set_text(options.target)
+      win1.set_text("exiimport_targetdir_entry", options.target)
    if len(args) > 0:
       win1.set_filelist(args)
    gtk.main()
