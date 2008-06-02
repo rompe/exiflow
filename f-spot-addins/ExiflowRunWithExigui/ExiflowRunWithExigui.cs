6a5c8cdf6ad31b2aadc8b0c3d37df263efc30eb3
Sebastian Berthold <sb@sleif.de>
2008-06-02 22:29:21 +0000

    first version

---
+++ f-spot-addins/ExiflowRunWithExigui/ExiflowRunWithExigui.cs
@@ -0,0 +1,46 @@
+/*
+ * ExiflowRunWithExigui.cs
+ *
+ * Author(s)
+ * 	Sebastian Berthold <exiflow@sleif.de>
+ *
+ * This is free software. See COPYING for details
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using Gtk;
+
+using FSpot;
+using FSpot.UI.Dialog;
+using FSpot.Extensions;
+
+namespace ExiflowRunWithExiguiExtension
+{
+	public class ExiflowRunWithExigui : ICommand
+	{
+		public void Run (object o, EventArgs e)
+		{
+			Console.WriteLine ("EXECUTING ExiflowRunWithExigui Extension");
+
+			string filelist = "";
+			
+			foreach (Photo p in MainWindow.Toplevel.SelectedPhotos ()) {
+				foreach (uint version_id in p.VersionIds) {
+					PhotoVersion pv = p.GetVersion (version_id) as PhotoVersion;
+					filelist = filelist + " " + pv.Uri.AbsolutePath;
+				}
+			Console.WriteLine (filelist);
+			}
+			System.Diagnostics.Process exigui = System.Diagnostics.Process.Start ("exigui", filelist); 
+				exigui.WaitForExit ();
+
+		}
+	}
+}
+
+
+
