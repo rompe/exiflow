14b59b42d921fd7244038e359ba44740d1d4e3fa
Sebastian Berthold <sb@sleif.de>
2008-07-29 20:59:38 +0000

    prepare file release

--- exiflow/debian/README.Debian.ex
+++
@@ -1,6 +0,0 @@
-exiflow for Debian
-------------------
-
-<possible notes regarding this package - if none, delete this file>
-
- -- Sebastian Berthold <exiflow@sleif.de>  Tue, 22 Jul 2008 22:45:15 +0200
