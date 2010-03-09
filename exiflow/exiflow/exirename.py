2f6b00fde0a24a2b716e11ea654ccc9f9efd38e2
Ulf Rompe <ulf@rompe.org>
2010-03-09 23:43:44 +0000

    Change indentation to the more standard 4 spaces.

--- exiflow/exiflow/exirename.py
+++ exiflow/exiflow/exirename.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# vim: tabstop=3 expandtab shiftwidth=3
+# vim: tabstop=4 expandtab shiftwidth=4
 """
 Rename a bunch of image files according to our holy file naming schema.
 
@@ -69,145 +69,145 @@ import exiflow.configfile
 
 
 def get_exif_information(filename):
-   """
-   Read camera model and date from filename and return them.
-   If The date isn't contained in EXIF, use the file's mtime.
-   """
-   exif_file = exiflow.exif.Exif(filename)
-   # read_exif may throw IOError. We leave the catching to our caller.
-   exif_file.read_exif()
-   model = exif_file.fields.get("Model", "all")
-   date = exif_file.fields.get("DateTimeOriginal", "0")
-   image_time = date
-   if ":" in date:
-      image_time = image_time[11:13] + image_time[14:16] + image_time[17:19]
-      date = date[0:4] + date[5:7] + date[8:10]
-   else:
-      if date == "0":
-         date = os.stat(filename).st_mtime
-         image_time = date
-      date = time.strftime("%Y%m%d", time.localtime(float(date)))
-      image_time = time.strftime("%H%M%S", time.localtime(float(image_time)))
-   return model, date, image_time
+    """
+    Read camera model and date from filename and return them.
+    If The date isn't contained in EXIF, use the file's mtime.
+    """
+    exif_file = exiflow.exif.Exif(filename)
+    # read_exif may throw IOError. We leave the catching to our caller.
+    exif_file.read_exif()
+    model = exif_file.fields.get("Model", "all")
+    date = exif_file.fields.get("DateTimeOriginal", "0")
+    image_time = date
+    if ":" in date:
+        image_time = image_time[11:13] + image_time[14:16] + image_time[17:19]
+        date = date[0:4] + date[5:7] + date[8:10]
+    else:
+        if date == "0":
+            date = os.stat(filename).st_mtime
+            image_time = date
+        date = time.strftime("%Y%m%d", time.localtime(float(date)))
+        image_time = time.strftime("%H%M%S", time.localtime(float(image_time)))
+    return model, date, image_time
 
 def get_new_filename_parts(filename, filelist):
-   """
-   Return a new name for filename according to our holy naming scheme.
-   """
-   leader, extension = os.path.splitext(filename)
-   extension = extension.lower()
-   number = "".join([char for char in leader[-4:] if char.isdigit()])
-   revision = "000"
-   # Look for high quality versions of this image. This is the case if we are
-   # a .jpg and more than one file exists with the same prefix.
-   if extension == ".jpg":
-      versions = [vers[0] for vers in filelist if vers[0].startswith(leader)]
-      if len(versions) > 1:
-         revision = "00l"
-   if not number:
-      raise IOError, "Can't find a number in " + filename
-   return number.zfill(4), revision, extension
+    """
+    Return a new name for filename according to our holy naming scheme.
+    """
+    leader, extension = os.path.splitext(filename)
+    extension = extension.lower()
+    number = "".join([char for char in leader[-4:] if char.isdigit()])
+    revision = "000"
+    # Look for high quality versions of this image. This is the case if we are
+    # a .jpg and more than one file exists with the same prefix.
+    if extension == ".jpg":
+        versions = [vers[0] for vers in filelist if vers[0].startswith(leader)]
+        if len(versions) > 1:
+            revision = "00l"
+    if not number:
+        raise IOError, "Can't find a number in " + filename
+    return number.zfill(4), revision, extension
 
 
 def rename_file(filename, filelist, with_time, cam_id_override=None,
                 artist_initials_override=None):
-   """
-   Rename filename and return the newly generated name without dir.
-   """
-   logger = logging.getLogger("exirename.rename_file")
-   filename_re = re.compile("^(\d{8})(-(\d{6}))?-(.{3})(\d{4})-((.{2})(.{3}))(\.[^.]*)$")
-   mymatch = filename_re.match(os.path.basename(filename))
-   """differentiate between getting values from existing filenames or getting initial 
-      values from exif and config file
-   """
-   if mymatch:
-      date = mymatch.group(1)
-      image_time = mymatch.group(3)
-      cam_id = mymatch.group(4)
-      number = mymatch.group(5)
-      artist_initials = mymatch.group(7)
-      revision = mymatch.group(8)
-      extension = mymatch.group(9)
-      if with_time:
-         if not image_time:
-            dummymodel, dummydate, image_time = get_exif_information(filename)
-         date += "-" + image_time
-   else:
-      model, date, image_time = get_exif_information(filename)
-      if with_time:
-         date += "-" + image_time
-      cam_id, artist_initials = exiflow.configfile.get_options("cameras", model,
+    """
+    Rename filename and return the newly generated name without dir.
+    """
+    logger = logging.getLogger("exirename.rename_file")
+    filename_re = re.compile("^(\d{8})(-(\d{6}))?-(.{3})(\d{4})-((.{2})(.{3}))(\.[^.]*)$")
+    mymatch = filename_re.match(os.path.basename(filename))
+    """differentiate between getting values from existing filenames or getting initial 
+       values from exif and config file
+    """
+    if mymatch:
+        date = mymatch.group(1)
+        image_time = mymatch.group(3)
+        cam_id = mymatch.group(4)
+        number = mymatch.group(5)
+        artist_initials = mymatch.group(7)
+        revision = mymatch.group(8)
+        extension = mymatch.group(9)
+        if with_time:
+            if not image_time:
+                dummymodel, dummydate, image_time = get_exif_information(filename)
+            date += "-" + image_time
+    else:
+        model, date, image_time = get_exif_information(filename)
+        if with_time:
+            date += "-" + image_time
+        cam_id, artist_initials = exiflow.configfile.get_options("cameras", model,
                                                     ("cam_id", "artist_initials"))
-      number, revision, extension = get_new_filename_parts(filename, filelist)
+        number, revision, extension = get_new_filename_parts(filename, filelist)
 
-   if cam_id_override:
-      cam_id = cam_id_override
+    if cam_id_override:
+        cam_id = cam_id_override
 
-   if artist_initials_override:
-      artist_initials = artist_initials_override
+    if artist_initials_override:
+        artist_initials = artist_initials_override
 
-   if len(cam_id) != 3 or len(artist_initials) != 2:
-      logger.warning("Either cam_id or artist_initials is missing or of wrong length. "
-                     "cam_id should be 3 characters and is currently set to '%s', "
-                     "artist_initials should be 2 characters and is set to '%s'. "
-                     "Skipping %s." % (cam_id, artist_initials, filename))
-      return os.path.basename(filename)
+    if len(cam_id) != 3 or len(artist_initials) != 2:
+        logger.warning("Either cam_id or artist_initials is missing or of wrong length. "
+                       "cam_id should be 3 characters and is currently set to '%s', "
+                       "artist_initials should be 2 characters and is set to '%s'. "
+                       "Skipping %s." % (cam_id, artist_initials, filename))
+        return os.path.basename(filename)
    
-   newbasename = date + "-" + cam_id + number + "-" + artist_initials + revision \
-                 + extension
-   newname = os.path.join(os.path.dirname(filename), newbasename)
-   if filename == newname:
-      raise IOError, "Filename does not change."
-   if os.path.exists(newname):
-      raise IOError, "Can't rename %s to %s, it already exists." % (filename, newname)
-   os.rename(filename, newname)
-   return newbasename
+    newbasename = date + "-" + cam_id + number + "-" + artist_initials + revision \
+                  + extension
+    newname = os.path.join(os.path.dirname(filename), newbasename)
+    if filename == newname:
+        raise IOError, "Filename does not change."
+    if os.path.exists(newname):
+        raise IOError, "Can't rename %s to %s, it already exists." % (filename, newname)
+    os.rename(filename, newname)
+    return newbasename
 
 
 def run(argv, callback=None):
-   """
-   Take an equivalent of sys.argv[1:] and optionally a callable.
-   Parse options, rename files and optionally call the callable on every
-   processed file with 3 arguments: filename, newname, percentage.
-   If the callable returns True, stop the processing.
-   """
-   parser = optparse.OptionParser(usage="usage: %prog [options] "
-                                        "<files or dirs>")
-   parser.add_option("--cam_id", "-c", dest="cam_id",
-                     help="ID string for the camera model. Should normally be"
-                          " three characters long.")
-   parser.add_option("--artist_initials", "-a", dest="artist_initials",
-                     help="Initials of the artist. Should be two characters"
-                          " long.")
-   parser.add_option("-t", "--with_time", action="store_true", dest="with_time",
-                     help="Create filenames containing the image time, for "
-                         "example 20071231-235959-n001234-xy000.jpg instead of "
-                         "20071231-n001234-xy000.jpg .")
-   parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
-                     help="Be verbose.")
-   options, args = parser.parse_args(args=argv)
+    """
+    Take an equivalent of sys.argv[1:] and optionally a callable.
+    Parse options, rename files and optionally call the callable on every
+    processed file with 3 arguments: filename, newname, percentage.
+    If the callable returns True, stop the processing.
+    """
+    parser = optparse.OptionParser(usage="usage: %prog [options] "
+                                         "<files or dirs>")
+    parser.add_option("--cam_id", "-c", dest="cam_id",
+                      help="ID string for the camera model. Should normally be"
+                           " three characters long.")
+    parser.add_option("--artist_initials", "-a", dest="artist_initials",
+                      help="Initials of the artist. Should be two characters"
+                           " long.")
+    parser.add_option("-t", "--with_time", action="store_true", dest="with_time",
+                      help="Create filenames containing the image time, for "
+                          "example 20071231-235959-n001234-xy000.jpg instead of "
+                          "20071231-n001234-xy000.jpg .")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                      help="Be verbose.")
+    options, args = parser.parse_args(args=argv)
 
-   logging.basicConfig(format="%(module)s: %(message)s")
-   if options.verbose:
-      logging.getLogger().setLevel(logging.INFO)
-   logger = logging.getLogger("exirename")
+    logging.basicConfig(format="%(module)s: %(message)s")
+    if options.verbose:
+        logging.getLogger().setLevel(logging.INFO)
+    logger = logging.getLogger("exirename")
 
-   filelist = exiflow.filelist.Filelist(args)
-   for filename, percentage in filelist:
-      try:
-         newname = rename_file(filename, filelist, options.with_time,
-                               options.cam_id, options.artist_initials)
-      except IOError, msg:
-         newname = os.path.basename(filename)
-         logger.error("Skipping %s:\n %s", filename, str(msg))
-      logger.info("%3s%% %s -> %s", percentage, filename, newname)
-      if callable(callback):
-         if callback(filename,
-                     os.path.join(os.path.dirname(filename), newname),
-                     percentage):
-            break
+    filelist = exiflow.filelist.Filelist(args)
+    for filename, percentage in filelist:
+        try:
+            newname = rename_file(filename, filelist, options.with_time,
+                                  options.cam_id, options.artist_initials)
+        except IOError, msg:
+            newname = os.path.basename(filename)
+            logger.error("Skipping %s:\n %s", filename, str(msg))
+        logger.info("%3s%% %s -> %s", percentage, filename, newname)
+        if callable(callback):
+            if callback(filename,
+                        os.path.join(os.path.dirname(filename), newname),
+                        percentage):
+                break
 
 
 if __name__ == "__main__":
-   run(sys.argv[1:])
+    run(sys.argv[1:])
 
