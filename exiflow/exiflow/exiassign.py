66641008d910d087947cfe556d0173f3e2366c1e
Ulf Rompe <ulf@rompe.org>
2010-03-09 23:14:05 +0000

    Change indentation to the more standard 4 spaces.

--- exiflow/exiflow/exiassign.py
+++ exiflow/exiflow/exiassign.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# vim: tabstop=3 expandtab shiftwidth=3
+# vim: tabstop=4 expandtab shiftwidth=4
 """
 Find groups of derived images and exchange EXIF information between them.
 
@@ -28,98 +28,98 @@ import exiflow.filelist
 
 
 def find_siblings(filename, prefix):
-   """
-   Try to find siblings for "filename" in the same directory and return them
-   as a list of files reverse sorted by modification time.
-   Return [] if no sibling is found.
-   A file is considered a sibling if it's basename starts with "prefix".
-   """
-   mtimes = {}
-   for otherfile in glob.glob(os.path.join(os.path.dirname(filename),
-                                           prefix + "*")):
-      if otherfile == filename:
-         continue
-      else:
-         mtimes[str(os.stat(otherfile).st_mtime) + otherfile] = otherfile
-   return [mtimes[mtime] for mtime in sorted(mtimes, reverse=True)]
+    """
+    Try to find siblings for "filename" in the same directory and return them
+    as a list of files reverse sorted by modification time.
+    Return [] if no sibling is found.
+    A file is considered a sibling if it's basename starts with "prefix".
+    """
+    mtimes = {}
+    for otherfile in glob.glob(os.path.join(os.path.dirname(filename),
+                                            prefix + "*")):
+        if otherfile == filename:
+            continue
+        else:
+            mtimes[str(os.stat(otherfile).st_mtime) + otherfile] = otherfile
+    return [mtimes[mtime] for mtime in sorted(mtimes, reverse=True)]
 
 
 def assign_file(filename, prefix, force=False):
-   """
-   Process "filename", passing "prefix" to find_siblings().
-   With force=True, force update even if EXIF is already present.
-   """
-   logger = logging.getLogger("exiassign.assign_file")
-   exif_file = exiflow.exif.Exif(filename)
-   try:
-      exif_file.read_exif()
-   except IOError, msg:
-      logger.warning(str(msg))
-      return 1
-   # Currently F-spot always tries to write "DateTimeOriginal", so we must change
-   # the key to detect files with valid exif information.
-   #if not force and exif_file.fields.has_key("DateTimeOriginal"):
-   if not force and exif_file.fields.has_key("Model"):
-      logger.info("Skipping %s, it seems to contain EXIF data.", filename)
-      return 0
-   for sibling in find_siblings(filename, prefix):
-      other_exif_file = exiflow.exif.Exif(sibling)
-      try:
-         other_exif_file.read_exif()
-      except IOError:
-         continue
-      other_exif_file.fields.update(exif_file.fields)
-      if other_exif_file.fields != exif_file.fields:
-         logger.info("Updating %s from %s.", filename, sibling)
-         exif_file.update_exif(sibling)
-         return 0
-   logger.info("No sibling found for %s.", filename)
-   return 0
+    """
+    Process "filename", passing "prefix" to find_siblings().
+    With force=True, force update even if EXIF is already present.
+    """
+    logger = logging.getLogger("exiassign.assign_file")
+    exif_file = exiflow.exif.Exif(filename)
+    try:
+        exif_file.read_exif()
+    except IOError, msg:
+        logger.warning(str(msg))
+        return 1
+    # Currently F-spot always tries to write "DateTimeOriginal", so we must change
+    # the key to detect files with valid exif information.
+    #if not force and exif_file.fields.has_key("DateTimeOriginal"):
+    if not force and exif_file.fields.has_key("Model"):
+        logger.info("Skipping %s, it seems to contain EXIF data.", filename)
+        return 0
+    for sibling in find_siblings(filename, prefix):
+        other_exif_file = exiflow.exif.Exif(sibling)
+        try:
+            other_exif_file.read_exif()
+        except IOError:
+            continue
+        other_exif_file.fields.update(exif_file.fields)
+        if other_exif_file.fields != exif_file.fields:
+            logger.info("Updating %s from %s.", filename, sibling)
+            exif_file.update_exif(sibling)
+            return 0
+    logger.info("No sibling found for %s.", filename)
+    return 0
 
 
 def run(argv, callback=None):
-   """
-   Take an equivalent of sys.argv[1:] and optionally a callable.
-   Parse options, assign relating files and gate meta information between them,
-   and optionally call the callable on every processed file with 3 arguments:
-   filename, newname, percentage.
-   If the callable returns True, stop the processing.
-   """
-   parser = optparse.OptionParser(usage="usage: %prog [options] "
-                                        "<files or dirs>")
-   parser.add_option("-f", "--force", action="store_true", dest="force",
-                     help="Force update even if EXIF is already present. "
-                          "The fields handled by these scripts are kept "
-                          "anyway.")
-   parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
-                     help="Be verbose.")
-   options, args = parser.parse_args(argv)
+    """
+    Take an equivalent of sys.argv[1:] and optionally a callable.
+    Parse options, assign relating files and gate meta information between them,
+    and optionally call the callable on every processed file with 3 arguments:
+    filename, newname, percentage.
+    If the callable returns True, stop the processing.
+    """
+    parser = optparse.OptionParser(usage="usage: %prog [options] "
+                                         "<files or dirs>")
+    parser.add_option("-f", "--force", action="store_true", dest="force",
+                      help="Force update even if EXIF is already present. "
+                           "The fields handled by these scripts are kept "
+                           "anyway.")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                      help="Be verbose.")
+    options, args = parser.parse_args(argv)
 
-   if len(args) == 0:
-      parser.print_help()
-      sys.exit(1)
+    if len(args) == 0:
+        parser.print_help()
+        sys.exit(1)
 
-   logging.basicConfig(format="%(module)s: %(message)s")
-   if options.verbose:
-      logging.getLogger().setLevel(logging.INFO)
-   logger = logging.getLogger("exiassign")
+    logging.basicConfig(format="%(module)s: %(message)s")
+    if options.verbose:
+        logging.getLogger().setLevel(logging.INFO)
+    logger = logging.getLogger("exiassign")
 
-   filelist = exiflow.filelist.Filelist(args)
+    filelist = exiflow.filelist.Filelist(args)
 
-   filename_re = re.compile("^(\d{8}(-\d{6})?-.{3}\d{4}-)(.{5})\.[^.]*$")
+    filename_re = re.compile("^(\d{8}(-\d{6})?-.{3}\d{4}-)(.{5})\.[^.]*$")
 
-   for filename, percentage in filelist:
-      mymatch = filename_re.match(os.path.basename(filename))
-      if mymatch:
-         logger.info("%3s%% %s", percentage, filename)
-         prefix = mymatch.groups()[0]
-         assign_file(filename, prefix, options.force)
-         if callable(callback):
-            if callback(filename, filename, percentage):
-               break
+    for filename, percentage in filelist:
+        mymatch = filename_re.match(os.path.basename(filename))
+        if mymatch:
+            logger.info("%3s%% %s", percentage, filename)
+            prefix = mymatch.groups()[0]
+            assign_file(filename, prefix, options.force)
+            if callable(callback):
+                if callback(filename, filename, percentage):
+                    break
 
 
 
 if __name__ == "__main__":
-   run(sys.argv[1:])
+    run(sys.argv[1:])
 
