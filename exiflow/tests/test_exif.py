7590535f0c2e9dabab9384df7c66001fdb753e4e
Sebastian Berthold <sb@sleif.de>
2010-03-23 22:24:26 +0000

    add read_exif imagedescription tests

--- exiflow/tests/test_exif.py
+++ exiflow/tests/test_exif.py
@@ -26,6 +26,14 @@ class TestExif(unittest.TestCase):
         self.__tempdir = tempfile.mkdtemp()
         self.__d70jpeg = os.path.join(self.__tempdir, "NikonD70.jpg")
         shutil.copy(os.path.join(sys.path[0], "NikonD70.jpg"), self.__tempdir)
+        self.__d70jpeg_imgdesc_latin1 = os.path.join(self.__tempdir,
+            "NikonD70_ImageDescription_2rows_latin1.jpg")
+        shutil.copy(os.path.join(sys.path[0], 
+            "NikonD70_ImageDescription_2rows_latin1.jpg"), self.__tempdir)
+        self.__d70jpeg_imgdesc_utf8 = os.path.join(self.__tempdir,
+            "NikonD70_ImageDescription_2rows_utf8.jpg")
+        shutil.copy(os.path.join(sys.path[0], 
+            "NikonD70_ImageDescription_2rows_utf8.jpg"), self.__tempdir)
         self.__emptyfile = os.path.join(self.__tempdir, "emptyfile.jpg")
         file(self.__emptyfile, 'w')
         self.__nosuchfile = os.path.join(self.__tempdir, "nosuchfile.jpg")
@@ -53,6 +61,27 @@ class TestExif(unittest.TestCase):
             exif.fields[field] = NikonD70.fields[field]
         self.failUnlessEqual(exif.fields, NikonD70.fields)
 
+    def test_read_exif_imagedescription(self):
+        """ Tests for read_exif() with valid image """
+        exif = exiflow.exif.Exif(self.__d70jpeg_imgdesc_utf8)
+        self.failUnless(isinstance(exif, exiflow.exif.Exif))
+        exif.read_exif()
+        for field in ("Directory", "FileModifyDate"):
+            exif.fields[field] = NikonD70.fields[field]
+        self.failUnlessEqual(exif.fields["ImageDescription"], 
+            u"ImageDescription first row äöüß\nImageDescription second row ÄÖÜß\n")
+
+    def test_read_exif_imagedescription_decode(self):
+        """ Tests for read_exif() with valid image """
+        exif = exiflow.exif.Exif(self.__d70jpeg_imgdesc_latin1)
+        self.failUnless(isinstance(exif, exiflow.exif.Exif))
+        exif.read_exif()
+        for field in ("Directory", "FileModifyDate"):
+            exif.fields[field] = NikonD70.fields[field]
+        self.failUnlessEqual(exif.fields["ImageDescription"], 
+            u"ImageDescription first row äöüß\nImageDescription second row ÄÖÜß\n")
+
+    
 
 if __name__ == '__main__':
     unittest.main()
