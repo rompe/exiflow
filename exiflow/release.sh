d207aac7a2e5f727eb22aedd660a39f5d3a0cd18
Ulf Rompe <ulf@rompe.org>
2010-10-07 21:14:36 +0000

    Prepare some files for Sourceforge uploads.

--- exiflow/release.sh
+++ exiflow/release.sh
@@ -11,16 +11,24 @@ else
 	dch -v ${version}-1 "Release Exiflow ${version}."
 	svn commit -m "Update debian changelog for ${version}." debian/changelog
 fi
+python setup.py bdist_rpm # For Sourceforge
 python setup.py sdist
-mv dist/Exiflow-${version}.tar.gz dist/exiflow_${version}.orig.tar.gz
+cp dist/Exiflow-${version}.tar.gz dist/exiflow_${version}.orig.tar.gz
 svn export . dist/exiflow-${version}
 cd dist/exiflow-${version}
 for dist in maverick lucid; do
 	dch --distribution ${dist} -b -v ${version}-1ppa1~${dist}1 "Upload Exiflow ${version} for ${dist}."
 	debuild -S -sa
+	debuild -b
 done
 cd ..
 rm -rf exiflow-${version}
-dput ppa:rompe/exiflow exiflow_${version}-1ppa1~maverick1_source.changes
-dput ppa:rompe/exiflow exiflow_${version}-1ppa1~lucid1_source.changes
+#dput ppa:rompe/exiflow exiflow_${version}-1ppa1~maverick1_source.changes
+#dput ppa:rompe/exiflow exiflow_${version}-1ppa1~lucid1_source.changes
+
+# Now some files for Sourceforge
+rm -rf sourceforge
+mkdir sourceforge
+mv *.rpm *.deb Exiflow-${version}.tar.gz sourceforge/
+echo "Now upload dist/sourceforge to Sourceforge."
 
