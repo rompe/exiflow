470e39c93b81142fc2af9e298433565bdb06b7a2
Ulf Rompe <ulf@rompe.org>
2015-05-05 15:20:30 +0200

    Add release script for copr.

---
+++ exiflow/release_copr.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+version=`grep -w "version=" setup.py | cut -d\' -f2`
+
+# Build RPM as well as src.rpm
+./setup.py bdist_rpm --build-requires=python,python-setuptools,perl-Image-ExifTool --requires=python,perl-Image-ExifTool
+# Test src.rpm in chroot like copr does
+mock --rebuild dist/Exiflow-$version-1.src.rpm
+
+# Upload src.rpm to web server
+publish_in_outgoing.sh dist/Exiflow-$version-1.src.rpm
+# Trigger copr build
+copr-cli build exiflow http://rompe.net/outgoing/Exiflow-$version-1.src.rpm
