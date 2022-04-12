3c06cf68732bfd8fede2f282adb837f258d16f48
Ulf Rompe <ulf@rompe.org>
2022-04-12 21:23:59 +0200

    Add type hints, fix warnings in gthumb.py

--- exiflow/exiflow/gthumb.py
+++ exiflow/exiflow/gthumb.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4
-"""
-Handle reading and writing gthumb comment files.
-"""
+"""Handle reading and writing gthumb comment files."""
 __revision__ = "$Id$"
 
 import os
 import re
 import gzip
+from typing import Dict, List
 import xml.dom.minidom
 
 
 class Gthumb(object):
-    """
-    A class that handles reading and writing gthumb comment files
-    for an image file.
-    """
+    """Read and write gthumb comment files for an image."""
 
-    def __init__(self, filename):
-        """
-        Create a Gthumb object for image filename.
-        """
-        self.fields = {}
+    def __init__(self, filename: str):
+        """Create a Gthumb object for image filename."""
+        self.fields: Dict[str, str] = {}
         self.filename = os.path.abspath(filename)
         self.commentsdir = os.path.join(os.path.dirname(self.filename),
                                         ".comments")
@@ -30,10 +24,11 @@ class Gthumb(object):
                                          os.path.basename(self.filename) +
                                          ".xml")
 
-    def read(self):
-        """
-        Read gthumb comment information for filename and fill the dictionary
-        self.fields with the corresponding EXIF values.
+    def read(self) -> bool:
+        r"""
+        Read gthumb comment information for filename.
+
+        Fill the dictionary self.fields with the corresponding EXIF values.
         Return True on success and False if no comment file exists.
 
         Translation of Exif values to gthumb sections:
@@ -57,20 +52,23 @@ class Gthumb(object):
                           "Keywords": "Keywords"}
 
         if os.path.isfile(self.commentsfile):
-            mydata = {}
-            mydom = xml.dom.minidom.parse(gzip.open(self.commentsfile))
+            mydata: Dict[str, str] = {}
+            mydom: xml.dom.minidom.Document = \
+                xml.dom.minidom.parse(  # type: ignore
+                    gzip.open(self.commentsfile))
             for field in ("Note", "Keywords", "Place", "Time"):
-                mynodes = mydom.getElementsByTagName(field)
+                mynodes: List[xml.dom.minidom.Element] = \
+                    mydom.getElementsByTagName(field)  # type: ignore
                 if len(mynodes) > 0:
                     mynodes = mynodes[0].childNodes
                     if len(mynodes) > 0:
-                        mydata[field] = mynodes[0].wholeText
+                        mydata[field] = mynodes[0].wholeText  # type: ignore
 
-            for key in gthumb_to_exif:
+            for key, value in gthumb_to_exif.items():
                 if key in mydata:
-                    self.fields[gthumb_to_exif[key]] = mydata[key]
+                    self.fields[value] = mydata[key]
             if "Note" in mydata:
-                note = []
+                note: List[str] = []
                 myregex = re.compile("(\\w+)::(.*)$")
                 for line in mydata["Note"].split("\n"):
                     match = myregex.match(line)
@@ -82,12 +80,13 @@ class Gthumb(object):
                     self.fields["XPTitle"] = note[0]
                     self.fields["ImageDescription"] = "\n".join(note).strip()
             return True
-        else:
-            return False
+        return False
 
-    def write(self, myaddfields=False, mytemplate=False):
+    def write(self, myaddfields: bool = False,
+              mytemplate: bool = False) -> None:
         """
         Write Exif information from self.fields into gthumb comment file.
+
         Apply reverse mapping of self.read().
 
         myaddfields: Write some additional fields.
@@ -95,18 +94,18 @@ class Gthumb(object):
         """
         exiffields = ["Artist", "Credit", "Copyright", "CopyrightNotice",
                       "UserComment"]
-        mydata = {}
+        mydata: Dict[str, str] = {}
         mydata["Place"] = self.fields.get("Location", "")
         mydata["Time"] = self.fields.get("DateTimeOriginal", "")
         mydata["Note"] = self.fields.get("ImageDescription", "")
-        tmpkeywords = []
+        tmpkeywords: List[str] = []
         for keyword in self.fields.get("Keywords", "").split(","):
             tmpkeywords.append(keyword.strip())
         mydata["Keywords"] = ",".join(tmpkeywords)
 
         # Add XPTitle to Note if Note doesn't contain it already
         mytitle = self.fields.get("XPTitle", "")
-        if len(mytitle) > 0 and not mytitle in mydata["Note"].splitlines():
+        if len(mytitle) > 0 and mytitle not in mydata["Note"].splitlines():
             mydata["Note"] = self.fields["XPTitle"] + "\n" + mydata["Note"]
         if myaddfields or mytemplate:
             mydata["Note"] += "\n"
@@ -116,32 +115,33 @@ class Gthumb(object):
                     mydata["Note"] += "\n" + field + ":: " + fieldvalue
 
         mydom = xml.dom.minidom.Document()
-        comment = mydom.createElement("Comment")
-        comment.setAttribute("format", "2.0")
-        mydom.appendChild(comment)
+        comment: xml.dom.minidom.Element = \
+            mydom.createElement("Comment")  # type: ignore
+        comment.setAttribute("format", "2.0")  # type: ignore
+        mydom.appendChild(comment)  # type: ignore
 
         for name in ("Place", "Time", "Note", "Keywords"):
-            element = mydom.createElement(name)
-            text = mydom.createTextNode(mydata[name])
-            element.appendChild(text)
-            comment.appendChild(element)
+            element = mydom.createElement(name)  # type: ignore
+            text = mydom.createTextNode(mydata[name])  # type: ignore
+            element.appendChild(text)  # type: ignore
+            comment.appendChild(element)  # type: ignore
 
         if not os.path.isdir(self.commentsdir):
             os.makedirs(self.commentsdir)
-        gzip.open(self.commentsfile, "wb").write(mydom.toxml(encoding="utf-8"))
+        gzip.open(self.commentsfile, "wb").write(
+            mydom.toxml(encoding="utf-8"))  # type: ignore
 
-    def get_mtime(self):
-        """
-        Get modification time of comment file or 0 if it doesn't exist.
-        """
+    def get_mtime(self) -> int:
+        """Get modification time of comment file or 0 if it doesn't exist."""
+        mtime = 0
         if os.path.isfile(self.commentsfile):
-            return os.path.getmtime(self.commentsfile)
-        else:
-            return 0
+            mtime = int(os.path.getmtime(self.commentsfile))
+        return mtime
 
-    def set_mtime(self, mtime):
+    def set_mtime(self, mtime: int) -> bool:
         """
         Set modification time of comment file to mtime.
+
         Returns True on success and False if no comment file exists.
         """
         if os.path.isfile(self.commentsfile):
@@ -153,6 +153,7 @@ class Gthumb(object):
     def cleanup(self):
         """
         Remove additional fields from gthumb comments.
+
         (other fields than Note, Keywords, Place, Time)
         Return True if a file is cleaned, False otherwise.
         """
