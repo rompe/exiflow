ec8428ff2958c4107b6ef33e41007c9dd8e4ee5a
Ulf Rompe <ulf@rompe.org>
2022-04-12 20:59:11 +0200

    Add type hints, fix warnings in filelist.py

--- exiflow/exiflow/filelist.py
+++ exiflow/exiflow/filelist.py
@@ -2,43 +2,43 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4
 """
-Generate lists of files given as names or searched in directories and
-provide an iterable interface to them.
+Generate lists of files given as names or searched in directories.
+
+Provide an iterable interface to them.
 """
 __revision__ = "$Id$"
 
 import os
 import time
 import logging
+from typing import Dict, Iterable, List
 from . import configfile
 
 
 class Filelist(object):
-    """
-    An iterable class that hosts information about files
+    """An iterable class that hosts information about files."""
 
-    given in it's contructor.
-    """
-
-    def __init__(self, pathes):
+    def __init__(self, pathes: Iterable[str]):
         """
         Create FileList object.
+
         Call add_files() with all arguments.
         If process_unknown_types is given, then no files will be skipped.
         """
-        self._files = []
-        self._skippedfiles = []
-        self._filedates = {}
-        self._filestats = {}
+        self._files: List[str] = []
+        self._skippedfiles: List[str] = []
+        self._filedates: Dict[str, str] = {}
+        self._filestats: Dict[str, os.stat_result] = {}
         self._fullsize = 0
         self._process_unknown_types = False
         for path in pathes:
             self.add_files([path])
 
-    def add_files(self, pathes):
+    def add_files(self, pathes: Iterable[str]) -> bool:
         """
-        Add filenames found in pathes either to _files or _skippedfiles,
-        depending on their extension and the value of _process_unknown_types.
+        Add filenames found in pathes either to _files or _skippedfiles.
+
+        (Depending on their extension and the value of _process_unknown_types.)
         Return False if files are skipped or no files are processed,
         True otherwise.
         Raises IOError on access errors.
@@ -63,11 +63,12 @@ class Filelist(object):
             else:
                 logger.warning("%s is not a regular file or directory. "
                                "Skipping.", path)
-        return bool(len(self._files)) and not bool(len(self._skippedfiles))
+        return bool(self._files and not self._skippedfiles)
 
-    def _add_file(self, filename):
+    def _add_file(self, filename: str) -> bool:
         """
         Add filename and all it's data to the filelist.
+
         Return True if the file is added, False otherwise.
         """
         logger = logging.getLogger("filelist._add_file")
@@ -77,7 +78,7 @@ class Filelist(object):
         basefilename = os.path.basename(filename).lower()
         if ((self._process_unknown_types
              or os.path.splitext(basefilename)[1] in image_extensions)
-                and not basefilename in unwanted_files):
+                and basefilename not in unwanted_files):
             self._files.append(filename)
             filestat = os.stat(filename)
             self._filestats[filename] = filestat
@@ -85,34 +86,38 @@ class Filelist(object):
             self._filedates[filename] = \
                 time.strftime("%Y-%m-%d", time.localtime(filestat.st_mtime))
             return True
-        else:
-            logger.info("Skipping %s.", filename)
-            self._skippedfiles.append(filename)
-            return False
+        logger.info("Skipping %s.", filename)
+        self._skippedfiles.append(filename)
+        return False
 
-    def process_unknown_types(self):
+    def process_unknown_types(self) -> None:
         """
-        Set _process_unknown_types to True, meaning that following method calls
-        should work on every file except those listed in "unwanted_files" in
-        the config file. Typical usage will be to call the constructor without
-        arguments, then call this method, and then call add_files().
+        Set _process_unknown_types to True.
+
+        This means that following method calls should work on every file except
+        those listed in "unwanted_files" in the config file. Typical usage will
+        be to call the constructor without arguments, then call this method,
+        and then call add_files().
         """
         self._process_unknown_types = True
 
     def __iter__(self):
         """
         Iterate through filelist and update percentage.
-        Returns (filename, percentage) pairs on each iteration.
+
+        Yield (filename, percentage) pairs on each iteration.
         """
         size_so_far = 0
         for filename in self._files:
             size_so_far += self._filestats[filename].st_size
             yield(filename, 100 * size_so_far / self._fullsize)
 
-    def get_daterange(self):
+    def get_daterange(self) -> str:
         """
-        Get the date range of the files as a string in the form
-        YYYY-MM-DD_-_YYYY-MM-DD   or  YYYY-MM-DD  if only one date available.
+        Get the date range of the files as a string.
+
+        The strings has the form  YYYY-MM-DD_-_YYYY-MM-DD
+        or  YYYY-MM-DD  if only one date available.
         """
         dates = sorted(set(self._filedates.values()))
         range_string = dates[0]
@@ -120,26 +125,18 @@ class Filelist(object):
             range_string += "_-_" + dates.pop()
         return range_string
 
-    def get_date(self, filename):
-        """
-        Get modification date for filename.
-        """
+    def get_date(self, filename: str) -> str:
+        """Get modification date for filename."""
         return self._filedates[filename]
 
     def get_fullsize(self):
-        """
-        Get cumulated bytes of files.
-        """
+        """Get cumulated bytes of files."""
         return self._fullsize
 
     def get_filecount(self):
-        """
-        Get number of files.
-        """
+        """Get number of files."""
         return len(self._files)
 
     def get_files(self):
-        """
-        Get al list of all files.
-        """
+        """Get al list of all files."""
         return self._files
