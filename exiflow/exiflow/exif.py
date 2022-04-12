7df8aef8ffc121feb1ba02ea72fcc983b3c9084a
Ulf Rompe <ulf@rompe.org>
2022-04-12 20:44:20 +0200

    Add type hints, fix warnings in exif.py

--- exiflow/exiflow/exif.py
+++ exiflow/exiflow/exif.py
@@ -1,140 +1,95 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4
-"""
-A module for reading and writing EXIF information.
-"""
+"""A module for reading and writing EXIF information."""
 __revision__ = "$Id$"
 
 import re
-import locale
 import logging
 import subprocess
+from typing import Dict
 
 
 class Exif(object):
-    """
-    A class for reading and writing EXIF information.
-    """
+    """A class for reading and writing EXIF information."""
 
-    def __init__(self, filename):
-        """
-        Create an Exif object for filename.
-        """
+    def __init__(self, filename: str):
+        """Create an Exif object for filename."""
         self.filename = filename
-        self.fields = {}
+        self.fields: Dict[str, str] = {}
 
-    def read_exif(self):
+    def read_exif(self) -> None:
         """
-        Read EXIF information from self.filename into self.fields
+        Read EXIF information from self.filename into self.fields.
+
         Raises IOError on errors.
         """
-        exiftool = subprocess.Popen(["exiftool", "-d", "%s", "-S",
-                                     self.filename],
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
-        stdout, stderr = exiftool.communicate()
+        with subprocess.Popen(["exiftool", "-d", "%s", "-S",
+                               self.filename],
+                              universal_newlines=True,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE) as exiftool:
+            stdout, stderr = exiftool.communicate()
         # exiftool doesn't necessarily use stderr on errors...
         for line in stdout.splitlines():
             if line.startswith("Error:"):
                 stderr += line
         if len(stderr) > 0:
             raise IOError(stderr)
-        else:
-            for line in stdout.splitlines():
-                if ": " in line:
-                    key, value = line.split(": ", 1)
-                    self.fields[key] = value.strip()
-            # We have to read the ImageDescription binary because it may
-            # contain things like line breaks.
-            self.fields["ImageDescription"] = \
-                "".join(subprocess.Popen(["exiftool", "-b",
-                                          "-ImageDescription",
-                                          self.filename],
-                                         stdout=subprocess.PIPE).stdout)
-            # We don't need an empty "ImageDescription" for any merge
-            # operations.
-            if self.fields["ImageDescription"] == "":
-                del self.fields["ImageDescription"]
-        # Be sure to have proper UTF8 strings
-        for key in self.fields:
-            self.fields[key] = self._decode(self.fields[key])
+        for line in stdout.splitlines():
+            if ": " in line:
+                key, value = line.split(": ", 1)
+                self.fields[key] = value.strip()
+        # We have to read the ImageDescription binary because it may
+        # contain things like line breaks.
+        with subprocess.Popen(["exiftool", "-b",
+                               "-ImageDescription",
+                               self.filename],
+                              universal_newlines=True,
+                              stdout=subprocess.PIPE) as exiftool:
+            self.fields["ImageDescription"] = exiftool.communicate()[0]
+        # We don't need an empty "ImageDescription" for any merge
+        # operations.
+        if self.fields["ImageDescription"] == "":
+            del self.fields["ImageDescription"]
 
-    @staticmethod
-    def _decode(field):
-        """
-        Decode string "field" by trying these encodings:
-
-        UTF-8, Current locale, Latin1
-
-        If none of these succeeds, just despair and return "???".
-        """
-        encodings = ['utf-8']
-        #
-        # next we add anything we can learn from the locale
-        try:
-            encodings.append(locale.nl_langinfo(locale.CODESET))
-        except AttributeError:
-            pass
-        try:
-            encodings.append(locale.getlocale()[1])
-        except (AttributeError, IndexError):
-            pass
-        try:
-            encodings.append(locale.getdefaultlocale()[1])
-        except (AttributeError, IndexError):
-            pass
-        #
-        # we try 'latin-1' last
-        encodings.append('latin-1')
-        for enc in encodings:
-            # some of the locale calls
-            # may have returned None
-            if not enc:
-                continue
-            try:
-                decoded = unicode(field, enc)
-            except (UnicodeError, LookupError):
-                decoded = unicode("???", "utf-8")
-            else:
-                break
-
-        return decoded
-
-    def write_exif(self):
+    def write_exif(self) -> int:
         """
         Write Exif Information from self.fields into self.filename.
+
         Raises IOError on errors.
         """
         logger = logging.getLogger("exif.write_exif")
         command = ["exiftool", "-overwrite_original", "-P"]
-        for field in self.fields.keys():
+        for field, value in self.fields.items():
             if field == "Keywords":
-                for keyword in self.fields[field].split(","):
-                    command.append("-%s=%s" % (field, keyword.strip()))
+                for keyword in value.split(","):
+                    command.append(f"-{field}={keyword.strip()}")
             elif field == "DateTimeOriginal":
                 # TODO: Writing back DateTimeOriginal seems to be a bad idea
                 # since gthumb drops seconds and there is some confusion with
                 # time strings and epoch.
                 continue
             else:
-                command.append("-%s=%s" % (field, self.fields[field]))
+                command.append(f"-{field}={self.fields[field]}")
         command.append(self.filename)
         logger.debug("Calling: %s", command)
-        exiftool = subprocess.Popen(command, stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
-        # exiftool doesn't reflect errors in it's return code, so we have to
-        # assume an error if something is written to stderr.
-        errors = exiftool.stderr.readlines()
-        if len(errors) > 0:
-            raise IOError("".join(errors + exiftool.stdout.readlines()))
-        return exiftool.wait()
+        with subprocess.Popen(command, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              universal_newlines=True) as exiftool:
+            stdout, stderr = exiftool.communicate()
+            result = exiftool.wait()
+        # exiftool doesn't reflect errors in it's return code, so we have
+        # to assume an error if something is written to stderr.
+        if len(stderr) > 0:
+            raise IOError(stderr + stdout)
+        return result
 
-    def update_exif(self, sourcefile):
+    def update_exif(self, sourcefile: str) -> int:
         """
-        Copy Exif Information from sourcefile into destfile and merge in values
-        from myexif. The fields used for merging have to be defined in
-        exiffields.
+        Copy Exif info from sourcefile to destfile, merge values from myexif.
+
+        The fields used for merging have to be defined in exiffields.
         Raises IOError on errors.
         """
         logger = logging.getLogger("exif.update_exif")
@@ -148,22 +103,24 @@ class Exif(object):
             if field in self.fields:
                 if field == "Keywords":
                     for keyword in self.fields[field].split(","):
-                        command.append("-%s=%s" % (field, keyword.strip()))
+                        command.append(f"-{field}={keyword.strip()}")
                 else:
-                    command.append("-%s=%s" % (field, self.fields[field]))
+                    command.append(f"-{field}={self.fields[field]}")
         command.append(self.filename)
         logger.debug("Calling: %s", command)
-        exiftool = subprocess.Popen(command, stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
+        with subprocess.Popen(command, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              universal_newlines=True) as exiftool:
+            stdout, stderr = exiftool.communicate()
+            result = exiftool.wait()
         # exiftool doesn't reflect errors in it's return code, so we have to
         # assume an error if something is written to stderr but ignore
         # warnings too.
-        errors = exiftool.stderr.readlines()
-        if len(errors) > 0:
+        if len(stderr) > 0:
             myregex = re.compile("^Warning: \\[minor\\] .* Fixed.")
-            mymatch = myregex.match(errors[0])
+            mymatch = myregex.match(stderr.splitlines()[0])
             if mymatch:
-                logger.info("%s", errors)
+                logger.info(stderr)
             else:
-                raise IOError("".join(errors + exiftool.stdout.readlines()))
-        return exiftool.wait()
+                raise IOError(stderr + stdout)
+        return result
